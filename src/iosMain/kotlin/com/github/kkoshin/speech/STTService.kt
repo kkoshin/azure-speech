53295a5a68f760064ae9ce69c316facf5b875256
Jiangc <jiangc@vistring.com>
2023-06-13 17:46:16 +0800

    feat: support iOS Stt service

--- src/iosMain/kotlin/com/github/kkoshin/speech/STTService.kt
+++ src/iosMain/kotlin/com/github/kkoshin/speech/STTService.kt
@@ -1,6 +1,11 @@
 package com.github.kkoshin.speech
 
 import MicrosoftCognitiveServicesSpeech.SPXAudioConfiguration
+import MicrosoftCognitiveServicesSpeech.SPXCancellationReason_Error
+import MicrosoftCognitiveServicesSpeech.SPXResultReason_NoMatch
+import MicrosoftCognitiveServicesSpeech.SPXResultReason_RecognizedSpeech
+import MicrosoftCognitiveServicesSpeech.SPXSpeechConfiguration
+import MicrosoftCognitiveServicesSpeech.SPXSpeechRecognizer
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -8,7 +13,36 @@ actual class STTService actual constructor(subscriptionKey: String, region: Stri
 
     private val TAG = this::class.simpleName
 
-    private val config = SPXAudioConfiguration()
+    private val speechRecognizer by lazy {
+        SPXSpeechRecognizer(SPXSpeechConfiguration(subscription = subscriptionKey, region = region).apply {
+            speechRecognitionLanguage = "en-US"
+        }, SPXAudioConfiguration()).also { reco ->
+            reco.addRecognizingEventHandler { _, e ->
+                _recognizing.tryEmit(e?.result?.text ?: "")
+            }
+
+            reco.addRecognizedEventHandler { _, e ->
+                if (e?.result?.reason == SPXResultReason_RecognizedSpeech) {
+                    _recognized.tryEmit(e.result.text)
+                } else if (e?.result?.reason == SPXResultReason_NoMatch) {
+                    _recognized.tryEmit(null)
+                }
+            }
+
+            reco.addCanceledEventHandler { _, e ->
+                if (e?.reason == SPXCancellationReason_Error) {
+                    println(
+                        "CANCELED: ErrorCode= ${e.errorCode} ErrorDetails= ${e.errorDetails}"
+                    )
+                }
+            }
+            reco.addSessionStoppedEventHandler { _, _ ->
+                println(
+                    "Session stopped event."
+                )
+            }
+        }
+    }
 
     private val _recognizing: MutableStateFlow<String> = MutableStateFlow("")
     actual val recognizing: Flow<String> = _recognizing
@@ -20,9 +54,10 @@ actual class STTService actual constructor(subscriptionKey: String, region: Stri
     actual val recognized: Flow<String?> = _recognized
 
     actual suspend fun startRecognize() {
+        speechRecognizer.startContinuousRecognition()
     }
 
     actual suspend fun stopRecognize() {
-
+        speechRecognizer.stopContinuousRecognition()
     }
 }
